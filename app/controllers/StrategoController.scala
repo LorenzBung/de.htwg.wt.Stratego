72ce3d48243c630ed4597752254a8a0feda94d89
unknown <pa981rei@FHKN.DE>
2019-10-14 16:55:56 +0200

    Add basic placing mechanisms

--- app/controllers/StrategoController.scala
+++ app/controllers/StrategoController.scala
@@ -6,8 +6,32 @@ import play.api.mvc._
 
 @Singleton
 class StrategoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  val gb = GameEngine.engine.gb
+  val eng = GameEngine.engine
   def stratego = Action {
-    Ok(gb.toString)
+    Ok(eng.gb.toString)
+  }
+  def newGame = Action {
+    GameEngine.engine.newGame()
+    Ok(eng.gb.toString)
+  }
+  def set(x: Int, y: Int) = Action {
+    val coords = de.htwg.se.stratego.model.boardComponent.Coordinates(x, y)
+    eng.set(coords)
+    Ok(eng.gb.toString)
+  }
+  def unset(x: Int, y: Int) = Action {
+    val coords = de.htwg.se.stratego.model.boardComponent.Coordinates(x, y)
+    eng.unset(coords)
+    Ok(eng.gb.toString)
+  }
+  def select(strength: Int) = Action {
+    eng.selectFigure(strength)
+    Ok(eng.gb.toString + "Selected Figure with strength " + strength)
+  }
+  def move(x1: Int, y1: Int, x2: Int, y2: Int) = Action {
+    val from = de.htwg.se.stratego.model.boardComponent.Coordinates(x1, y1)
+    val to = de.htwg.se.stratego.model.boardComponent.Coordinates(x2, y2)
+    eng.move(from, to)
+    Ok(eng.gb.toString)
   }
 }
