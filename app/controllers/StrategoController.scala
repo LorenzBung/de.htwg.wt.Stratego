fe45006d1703c475273d94cd807314134e9fec5d
Lorenz Bung <lorenz.bung@htwg-konstanz.de>
2019-10-16 09:48:01 +0200

    Add HTML views and about page

--- app/controllers/StrategoController.scala
+++ app/controllers/StrategoController.scala
@@ -8,30 +8,33 @@ import play.api.mvc._
 class StrategoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val eng = GameEngine.engine
   def stratego = Action {
-    Ok(eng.gb.toString)
+    Ok(views.html.stratego(eng))
   }
   def newGame = Action {
     GameEngine.engine.newGame()
-    Ok(eng.gb.toString)
+    Ok(views.html.stratego(eng))
   }
   def set(x: Int, y: Int) = Action {
     val coords = de.htwg.se.stratego.model.boardComponent.Coordinates(x, y)
     eng.set(coords)
-    Ok(eng.gb.toString)
+    Ok(views.html.stratego(eng))
   }
   def unset(x: Int, y: Int) = Action {
     val coords = de.htwg.se.stratego.model.boardComponent.Coordinates(x, y)
     eng.unset(coords)
-    Ok(eng.gb.toString)
+    Ok(views.html.stratego(eng))
   }
   def select(strength: Int) = Action {
     eng.selectFigure(strength)
-    Ok(eng.gb.toString + "Selected Figure with strength " + strength)
+    Ok(views.html.stratego(eng))
   }
   def move(x1: Int, y1: Int, x2: Int, y2: Int) = Action {
     val from = de.htwg.se.stratego.model.boardComponent.Coordinates(x1, y1)
     val to = de.htwg.se.stratego.model.boardComponent.Coordinates(x2, y2)
     eng.move(from, to)
-    Ok(eng.gb.toString)
+    Ok(views.html.stratego(eng))
+  }
+  def about = Action {
+    Ok(views.html.about())
   }
 }
